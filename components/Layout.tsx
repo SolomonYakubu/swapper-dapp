0ac3ed826e43b77a9cb0748f6b816ef67d20104d
Solomon Yakubu <soloyaks.sy@gmail.com>
2023-02-04 02:14:17 +0100

    first commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 const Layout = ({ children }: any) => {
   return (
-    <div className="min-h-[calc(100vh-64px)] h-[calc(100vh-62px)]  p-0 m-0 mt-16">
+    <div className="min-h-[calc(100vh-64px)] h-[calc(100vh-62px)]  p-0 m-0">
       {children}
     </div>
   );
