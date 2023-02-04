59a8bdffc117d221415bd188e02d52dadc6af5e8
Solomon Yakubu <soloyaks.sy@gmail.com>
2023-02-04 02:06:48 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Joker World</title>
+        <title>Swapper - Swap Dapp</title>
         <style>
           @import
           url(&quot;https://fonts.googleapis.com/css2?family=Questrial&family=Questrial&display=swap)
         </style>
-        <meta property="og:title" content="Joker World" />
+        <meta property="og:title" content="Swapper - Swap Dapp" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -31,13 +31,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="favicon.ico"></link>
         <link rel="shortcut icon" href="favicon.ico" />
 
-        <meta
-          property="og:description"
-          content="JOKER WORLD is a deflationary DEFI project that implements a
-                  unique tax system and utility, we are here to create a new
-                  chapter for DEFI. Read our whitepaper to learn more about our
-                  ultimate machine"
-        />
+        <meta property="og:description" content="" />
         <meta property="og:url" content="" />
       </Head>
       <Component {...pageProps} />
